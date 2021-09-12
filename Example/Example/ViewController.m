288a5dcfadc4b58a18354e42ad655b0dcfe968bd
Huang-Libo <LiboHwang+IT@gmail.com>
2021-09-12 19:41:57 +0800

    Add NSLog demo

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -6,6 +6,9 @@
 //
 
 #import "ViewController.h"
+#import <fishhook/fishhook.h>
+
+static void (*orig_NSLog)(NSString *format, ...);
 
 @interface ViewController ()
 
@@ -13,9 +16,29 @@
 
 @implementation ViewController
 
+void my_NSLog(NSString *format, ...) {
+    if(!format) {
+        return;
+    }
+    NSString *extra = @"[hook NSLog] ";
+    format = [extra stringByAppendingString:format];
+    va_list args;
+    va_start(args, format);
+    NSString *message = [[NSString alloc] initWithFormat:format arguments:args];
+    orig_NSLog(@"%@", message);
+    va_end(args);
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSLog(@"Before hook NSLog\n");
+    struct rebinding rebindings[1] = {
+        {"NSLog", my_NSLog, (void *)&orig_NSLog}
+    };
+    rebind_symbols(rebindings, 1);
+    NSLog(@"After hook NSLog\n");
 }
 
 
