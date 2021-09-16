c953af3d655320be2caebdd80c05031b2ea27aff
Huang-Libo <LiboHwang+IT@gmail.com>
2021-09-16 17:21:03 +0800

    添加注释。

--- fishhook.c
+++ fishhook.c
@@ -126,19 +126,33 @@ static int get_protection(void *addr, vm_prot_t *prot, vm_prot_t *max_prot) {
 /// @param rebindings 单链表的头结点
 /// @param section 符号所在的 section
 /// @param slide 偏移量
-/// @param symtab 符号表
-/// @param strtab 字符串表
-/// @param indirect_symtab Dynamic Symbol Table (Indirect Symbols)
+/// @param symtab `符号表`的地址
+/// @param strtab `字符串表`的地址
+/// @param indirect_symtab Dynamic Symbol Table (Indirect Symbols) 的地址
 static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
                                            section_t *section,
                                            intptr_t slide,
                                            nlist_t *symtab,
                                            char *strtab,
                                            uint32_t *indirect_symtab) {
-  // reserved1
+  // 解读: load command 的 Section64 Header (类型是 struct section_64 ) 的
+  //      reserved1 字段表示 symbol 在 indirect_symtab 中的偏移量
+  // 获取 symbol 的地址, 这个地址上默认是 `dyld_stub_binder` 符号
   uint32_t *indirect_symbol_indices = indirect_symtab + section->reserved1;
+  // section->addr 表示当前 load command Section64 Header 对应的 __DATA 段的 section 的地址
+  // 加上偏移量就是 __DATA 段的 section 的实际地址
+  // 这里取二级指针, 即指针的指针, 可理解成存放指针的数组, 即这个数组中存放的是函数指针
+  // 比如数组: void **atr = [*ptr1, *ptr2, *ptr3, ...] ,
+  //         数组的元素是指针, 因此这个数组需要使用二级指针表示.
+  // indirect_symbol_bindings 是存储 Lazy/Non-Lazy Symbol Pointer 的数组
   void **indirect_symbol_bindings = (void **)((uintptr_t)slide + section->addr);
 
+  // 要执行重绑定的 section 可能是
+  // 1. (__DATA_CONST,__got)
+  // 2. (__DATA,__la_symbol_ptr)
+  // 这两个 section 中存储的都是指针
+  // section->size / sizeof(void *) : 计算当前 section 中指针的个数
+  // 遍历 section (里面都是指针数组)中的指针
   for (uint i = 0; i < section->size / sizeof(void *); i++) {
     uint32_t symtab_index = indirect_symbol_indices[i];
     if (symtab_index == INDIRECT_SYMBOL_ABS || symtab_index == INDIRECT_SYMBOL_LOCAL ||
@@ -146,15 +160,30 @@ static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
       continue;
     }
     uint32_t strtab_offset = symtab[symtab_index].n_un.n_strx;
+    // 在字符串表中获取符号的名称
     char *symbol_name = strtab + strtab_offset;
+    // C 函数对应的符号名是以下划线开头的, 比如 _printf
     bool symbol_name_longer_than_1 = symbol_name[0] && symbol_name[1];
+    // cur 指向单链表的头结点
     struct rebindings_entry *cur = rebindings;
-    while (cur) {
-      for (uint j = 0; j < cur->rebindings_nel; j++) {
-        if (symbol_name_longer_than_1 && strcmp(&symbol_name[1], cur->rebindings[j].name) == 0) {
+    // 遍历单链表, 查找与上面的 symbol_name 匹配的项
+    while (cur)
+    {
+      // 遍历链表结点中的 struct rebinding 数组
+      for (uint j = 0; j < cur->rebindings_nel; j++)
+      {
+        // 比较名称时, 不包含下划线, 因此索引从 1 开始
+        if (symbol_name_longer_than_1 &&
+            strcmp(&symbol_name[1], cur->rebindings[j].name) == 0)
+        { // 找到了指定的符号
           kern_return_t err;
 
-          if (cur->rebindings[j].replaced != NULL && indirect_symbol_bindings[i] != cur->rebindings[j].replacement) {
+          if (cur->rebindings[j].replaced != NULL &&
+              indirect_symbol_bindings[i] != cur->rebindings[j].replacement)
+          {
+            // 记录原函数指针
+            // indirect_symbol_bindings[i] 是从二级指针中取出原函数指针
+            // (或理解成从存放有函数指针的数组中取出某个函数指针)
             *(cur->rebindings[j].replaced) = indirect_symbol_bindings[i];
           }
 
@@ -165,7 +194,12 @@ static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
            *    API on some iOS/Mac reports mismatch vm protection attributes.
            * -- Lianfu Hao Jun 16th, 2021
            **/
-          err = vm_protect (mach_task_self (), (uintptr_t)indirect_symbol_bindings, section->size, 0, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+          // 修改 Lazy/Non-Lazy Symbol Pointer 所在区域的权限.
+          // (在某些系统版本上是只读的, 需要改成可读可写)
+          err = vm_protect (mach_task_self (),
+                            (uintptr_t)indirect_symbol_bindings, section->size,
+                            0,
+                            VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
           if (err == KERN_SUCCESS) {
             /**
              * Once we failed to change the vm protection, we
@@ -173,11 +207,14 @@ static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
              * iOS 15 has corrected the const segments prot.
              * -- Lionfore Hao Jun 11th, 2021
              **/
+            // 最终: 将原函数替换成我们自定义的函数
             indirect_symbol_bindings[i] = cur->rebindings[j].replacement;
           }
+          // 从第三层循环跳回到第一层循环
           goto symbol_loop;
         }
       }
+      // 指向链表的下一个节点
       cur = cur->next;
     }
   symbol_loop:;
@@ -247,21 +284,27 @@ static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
 
   // cur 指针又重置到 load command 的起始位置
   cur = (uintptr_t)header + sizeof(mach_header_t);
+  // 再次遍历 load command
   for (uint i = 0; i < header->ncmds; i++, cur += cur_seg_cmd->cmdsize) {
     cur_seg_cmd = (segment_command_t *)cur;
     if (cur_seg_cmd->cmd == LC_SEGMENT_ARCH_DEPENDENT) {
-      // 判断当前是否是 __DATA 或 __DATA_CONST 的 load command , 如果都不是则 continue 循环
+      // 判断当前是否是 __DATA 或 __DATA_CONST 的 load command , 如果都不是则 continue
+      // 1. SEG_DATA : __DATA
+      // 2. SEG_DATA_CONST : __DATA_CONST
       if (strcmp(cur_seg_cmd->segname, SEG_DATA) != 0 &&
           strcmp(cur_seg_cmd->segname, SEG_DATA_CONST) != 0) {
         continue;
       }
-      // nsects: number of sections in segment
+      // nsects : number of sections in segment
       for (uint j = 0; j < cur_seg_cmd->nsects; j++) {
+        // 获取当前 load command 下的第 j 个 Section64 Header , 对应 struct section_64 结构体类型
         section_t *sect = (section_t *)(cur + sizeof(segment_command_t)) + j;
-        if ((sect->flags & SECTION_TYPE) == S_LAZY_SYMBOL_POINTERS) { // 找到 __la_symbol_ptr 中的 Lazy Symbol Pointer
+        if ((sect->flags & SECTION_TYPE) == S_LAZY_SYMBOL_POINTERS) {
+          // 找到 __la_symbol_ptr 中的 Lazy Symbol Pointer
           perform_rebinding_with_section(rebindings, sect, slide, symtab, strtab, indirect_symtab);
         }
-        if ((sect->flags & SECTION_TYPE) == S_NON_LAZY_SYMBOL_POINTERS) {// 找到 __got 中的 Non-Lazy Symbol Pointer
+        if ((sect->flags & SECTION_TYPE) == S_NON_LAZY_SYMBOL_POINTERS) {
+          // 找到 __got 中的 Non-Lazy Symbol Pointer
           perform_rebinding_with_section(rebindings, sect, slide, symtab, strtab, indirect_symtab);
         }
       }
@@ -301,13 +344,18 @@ int rebind_symbols(struct rebinding rebindings[], size_t rebindings_nel) {
   }
   // If this was the first call, register callback for image additions (which is also invoked for
   // existing images, otherwise, just run on existing images
-  if (!_rebindings_head->next) { // 单链表中只有一个节点时, 说明是第一次调用 `rebind_symbols()` , 因此需要调用 `_dyld_register_func_for_add_image()` 注册回调函数
-    // 文档: During a call to `_dyld_register_func_for_add_image()` the callback func is called for every existing image. Later, it is called as each new image is loaded and bound
+  if (!_rebindings_head->next) {
+    // 1. 单链表中只有一个节点时, 说明是第一次调用 `rebind_symbols()` ,
+    //    因此需要调用 `_dyld_register_func_for_add_image()` 注册回调函数
+    // 文档: During a call to `_dyld_register_func_for_add_image()` the callback func
+    //      is called for every existing image. Later, it is called as each new image
+    //      is loaded and bound
     // 解读: 在调用 `_dyld_register_func_for_add_image()` 期间，会为每个现有的 image 调用回调函数。
     //      此后, 在加载和绑定每个新 image 时调用该回调函数.
     // 问题: dyld 怎么给这个回调函数传参的?
     _dyld_register_func_for_add_image(_rebind_symbols_for_image);
-  } else { // 单链表中有多个元素, 说明不是第一次调用 `rebind_symbols()` , 此时需要对以加载的 image 执行重绑定
+  } else {
+    // 2. 单链表中有多个元素, 说明不是第一次调用 `rebind_symbols()` , 此时需要对以加载的 image 执行重绑定
     uint32_t c = _dyld_image_count();
     for (uint32_t i = 0; i < c; i++) {
       _rebind_symbols_for_image(_dyld_get_image_header(i), _dyld_get_image_vmaddr_slide(i));
